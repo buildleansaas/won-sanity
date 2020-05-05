ea3bf5e261b5543f2b4261bbd51a61784df0be55
awitherow <au.witherow@gmail.com>
2020-05-04 14:23:01 -1000

    add video schema

---
+++ schemas/_video.js
@@ -0,0 +1,22 @@
+export const video = {
+  title: "Video",
+  name: "video",
+  type: "object",
+  fields: [
+    {
+      title: "Youtube Embed Link",
+      name: "embedLink",
+      type: "string",
+    },
+    {
+      title: "Title",
+      name: "title",
+      type: "string",
+    },
+    {
+      title: "About This Video",
+      name: "description",
+      type: "string",
+    },
+  ],
+};
--- schemas/schema.js
+++ schemas/schema.js
@@ -1,17 +1,13 @@
-import createSchema from 'part:@sanity/base/schema-creator'
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import createSchema from "part:@sanity/base/schema-creator";
+import schemaTypes from "all:part:@sanity/base/schema-type";
 
-import { scheduleItem } from "./_scheduleItem"
-import { event } from "./_event"
-import { location } from "./_location"
-import { timeslot } from "./_timeSlot"
+import { scheduleItem } from "./_scheduleItem";
+import { event } from "./_event";
+import { location } from "./_location";
+import { timeslot } from "./_timeSlot";
+import { video } from "./_video";
 
 export default createSchema({
-  name: 'default',
-  types: schemaTypes.concat([
-    scheduleItem,
-    event,
-    location,
-    timeslot
-  ])
-})
+  name: "default",
+  types: schemaTypes.concat([scheduleItem, event, location, timeslot, video]),
+});
